df98466f7b3c817a677e1d3fa66b696195857cac
sindhu-inuganti <sinuganti@cardlytics.com>
2020-02-19 10:44:51 +0530

    initial commit

---
+++ Employee_form/form_validation.js
@@ -0,0 +1,13 @@
+function getStates()
+
+{
+    var country=document.getElementById("country").value;
+    var States={"India":["Kerala","Telangana","Andhra Pradesh"],"Australia":["Queensland","Tasmania"]};
+    states="";
+    st=States[country];
+    for(var i=0;i<st.length;i++)
+    {
+        
+    }
+
+}
