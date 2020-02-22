cae5690c5da7b02448aeac3f26c77d3a3f12b30e
sindhu-inuganti <sinuganti@cardlytics.com>
2020-02-23 01:09:23 +0530

    initial commit

---
+++ tryFunctions.js
@@ -0,0 +1,15 @@
+//function myFunc(a,b)
+//function myFunc()
+function myFunc(a=10,b=10)
+{
+    //var a=10, b=20;
+    return (a+b)
+    
+}
+
+//console.log(myFunc())
+
+// console.log(myFunc(7))
+// console.log(myFunc(4,5))
+// console.log(myFunc(4,5,6))
+
