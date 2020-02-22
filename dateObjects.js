f253ffb9d18bc9c3a5a4946321357b955ad446b7
sindhu-inuganti <sinuganti@cardlytics.com>
2020-02-23 00:43:34 +0530

    initial commit

---
+++ dateObjects.js
@@ -0,0 +1,28 @@
+var d = new Date();
+console.log(d)
+
+// d=new Date(2018)
+// console.log(d)
+
+// d=new Date(2018,11)
+// console.log(d)
+
+// d=new Date(2018,11,24)
+// console.log(d)
+
+// d=new Date(2018,11,24,10)
+// console.log(d)
+
+// d = new Date(-100000000000);
+// console.log(d)
+
+//console.log(d.toUTCString())
+
+// console.log(d.getDate())
+// console.log(d.getFullYear())
+// console.log(d.getUTCHours())
+
+// d.setDate(19)
+// console.log(d)
+
+
