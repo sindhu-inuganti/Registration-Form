91c18f78265185c10fd88e74c857d6cd0dfc7fee
sindhu-inuganti <sinuganti@cardlytics.com>
2020-02-24 11:28:57 +0530

    Call, Apply, Bind

--- tryFunctions.js
+++ tryFunctions.js
@@ -1,9 +1,9 @@
 //function myFunc(a,b)
 //function myFunc()
-function myFunc(a=10,b=10)
+var funcVar = function myFunc(a,b)
 {
     //var a=10, b=20;
-    return (a+b)
+    return (this.x+this.y+a+b)
     
 }
 
@@ -12,4 +12,18 @@ function myFunc(a=10,b=10)
 // console.log(myFunc(7))
 // console.log(myFunc(4,5))
 // console.log(myFunc(4,5,6))
+
+var data= {x:15,y:15}
+console.log(funcVar.call(data,4,5));//call
+
+var arr=[2,3]
+console.log(funcVar.apply(data,arr))//apply
+console.log(funcVar.apply(data,[4,5]))//apply
+
+
+
+
+
+
+
 
